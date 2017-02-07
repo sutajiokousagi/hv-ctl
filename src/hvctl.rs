8f2cea102013f9df4b535692635fdb109e9f8208
bunnie <bunnie@kosagi.com>
2017-02-07 17:53:38 +0000

    refactor code to give hvctl its own abstraction layer

---
+++ src/hvctl.rs
@@ -0,0 +1,168 @@
+use std::fmt;
+use cupi_shift::Shifter;
+use cupi::CuPi;
+use cupi::PinOutput;
+use cupi::DigitalWrite;
+
+const BRD_SRCLK: usize = 22;
+const BRD_RCLK: usize = 21;
+const BRD_SRDOUT: usize  = 24;
+const BRD_SRCLR: usize = 23;  // clear when high
+
+const BRD_LOCKOUT_N: usize = 29;
+
+enum_from_primitive! {
+    #[derive(Debug, PartialEq, Copy, Clone)]
+    pub enum HvCtl {
+        HvEngage =    0b10000000,
+        HvgenEna =    0b01000000,
+        Sel300Ohm =   0b00100000,
+        Sel620Ohm =   0b00010000,
+        Sel750Ohm =   0b00001000,
+        Sel1000Ohm =  0b00000100,
+        SelHicap =    0b00000010,
+        SelLocap =    0b00000001,
+    }
+}
+
+impl fmt::Display for HvCtl {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            HvCtl::HvEngage => write!(f, "HvEngage"),
+            HvCtl::HvgenEna => write!(f, "HvgenEna"),
+            HvCtl::Sel300Ohm => write!(f, "Sel300Ohm"),
+            HvCtl::Sel620Ohm => write!(f, "Sel620Ohm"),
+            HvCtl::Sel750Ohm => write!(f, "Sel750Ohm"),
+            HvCtl::Sel1000Ohm => write!(f, "Sel1000Ohm"),
+            HvCtl::SelHicap => write!(f, "SelHicap"),
+            HvCtl::SelLocap => write!(f, "SelLocap"),
+        }
+    }
+}
+
+enum_from_primitive! {
+    #[derive(Debug, PartialEq, Copy, Clone)]
+    pub enum RowSel {
+        RowNone = 0,
+        RowSel1 = 0b10000000,
+        RowSel2 = 0b01000000,
+    }
+}
+
+impl fmt::Display for RowSel {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            RowSel::RowNone => write!(f, "No Row"),
+            RowSel::RowSel1 => write!(f, "RowSel1"),
+            RowSel::RowSel2 => write!(f, "RowSel2"),
+        }
+    }
+}
+
+enum_from_primitive! {
+    #[derive(Debug, PartialEq, Copy, Clone)]
+    pub enum ColSel {
+        ColNone = 0,
+        ColSel1 = 0b10000000,
+        ColSel2 = 0b01000000,
+    }
+}
+
+impl fmt::Display for ColSel {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            ColSel::ColNone => write!(f, "No Column"),
+            ColSel::ColSel1 => write!(f, "ColSel1"),
+            ColSel::ColSel2 => write!(f, "ColSel2"),
+        }
+    }
+}
+
+enum_from_primitive! {
+    #[derive(Debug, PartialEq, Copy, Clone)]
+    pub enum HvEngage {
+        HvGenOn = 1,
+        HvGenOff = 0,
+    }
+}
+
+impl fmt::Display for HvEngage {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            HvEngage::HvGenOn => write!(f, "HV generator on"),
+            HvEngage::HvGenOff => write!(f, "HV generator off"),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum HvErr {
+    None,
+}
+
+pub struct HvConfig {
+    initialized: bool,
+    shifter: Shifter,
+    colsel: usize,
+    rowsel: usize,
+    hvsel: usize,
+    pin_lockout_n: PinOutput,
+    pin_srclr: PinOutput,
+}
+
+impl HvConfig {
+    pub fn new(cupi: CuPi) -> Result<HvConfig,HvErr> {
+        let (data_pin, latch_pin, clock_pin) = (BRD_SRDOUT, BRD_RCLK, BRD_SRCLK);
+        let mut hvcfg = HvConfig {
+            initialized: true,
+            shifter: Shifter::new(data_pin, latch_pin, clock_pin),
+            colsel: 0,
+            rowsel: 0,
+            hvsel: 0,
+            pin_lockout_n: cupi.pin(BRD_LOCKOUT_N).unwrap().output(),
+            pin_srclr: cupi.pin(BRD_SRCLR).unwrap().output(),
+        };
+
+        hvcfg.colsel = hvcfg.shifter.add(8);
+        hvcfg.rowsel = hvcfg.shifter.add(8);
+        hvcfg.hvsel = hvcfg.shifter.add(8);
+        
+        hvcfg.shifter.set(hvcfg.rowsel, 0, false);
+        hvcfg.shifter.set(hvcfg.colsel, 0, false);
+        hvcfg.shifter.set(hvcfg.hvsel, 0, true);
+
+        hvcfg.pin_lockout_n.low().unwrap(); // force lockout to be active
+        hvcfg.pin_srclr.low().unwrap(); // don't be clearing the shift registers
+
+        Ok(hvcfg)
+    }
+
+    pub fn cleanup(&mut self) {
+        if self.initialized {
+            self.initialized = false;
+        }
+    }
+
+    pub fn update_ctl(&mut self, val: u8, hvon: HvEngage) {
+        self.shifter.set(self.hvsel, val as usize, true);
+        if hvon == HvEngage::HvGenOn {
+            self.pin_lockout_n.high().unwrap();
+        } else {
+            self.pin_lockout_n.low().unwrap();
+        }
+    }
+
+    pub fn update_rowsel(&mut self, val: RowSel) {
+        self.shifter.set(self.rowsel, val as usize, true);
+    }
+
+    pub fn update_colsel(&mut self, val: ColSel) {
+        self.shifter.set(self.colsel, val as usize, true);
+    }
+}
+
+impl Drop for HvConfig {
+    fn drop(&mut self) {
+        self.cleanup();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,79 @@
 extern crate cupi;
 extern crate cupi_shift;
 
-mod slewrate;
 mod system;
+
+mod slewrate;
 use slewrate::*;
+
+mod hvctl;
+use hvctl::*;
 
 #[macro_use]
 extern crate quick_error;
 
-use cupi::{CuPi, delay_ms, DigitalWrite};
-use cupi_shift::Shifter;
+#[macro_use] extern crate enum_primitive;
 
-const BRD_LOCKOUT_N: usize = 29;
+use cupi::CuPi;
+use cupi::delay_ms;
+
 //const BRD_MOSI: usize = ;
 //const BRD_MISO: usize = ;
 //const BRD_CLK: usize = ;
 //const BRD_ADC_CS_N: usize = ;
 //const BRD_DAC_CS_N: usize = ;
 
-const BRD_SRCLK: usize = 22;
-const BRD_RCLK: usize = 21;
-const BRD_SRDOUT: usize  = 24;
-const BRD_SRCLR: usize = 23;  // clear when high
-
 fn main() {
     let cupi = CuPi::new().unwrap();
 
-    let mut pin_lockout_n = cupi.pin(BRD_LOCKOUT_N).unwrap().output();
-    pin_lockout_n.low().unwrap(); // force lockout to be active
-
-    let mut pin_srclr = cupi.pin(BRD_SRCLR).unwrap().output();
-    pin_srclr.low().unwrap(); // don't be clearing the shift registers
-
+    let mut hvcfg = HvConfig::new(cupi).unwrap();
+    
     slewrate().unwrap();
-    
-    let (data_pin, latch_pin, clock_pin) = (BRD_SRDOUT, BRD_RCLK, BRD_SRCLK);
 
-    let mut shifter = Shifter::new(data_pin, latch_pin, clock_pin);
-
-    let colsel = shifter.add(8);
-    let rowsel = shifter.add(8);
-    let hvctl = shifter.add(8);
-    
-    shifter.set(rowsel, 0, true);
-    shifter.set(colsel, 0, true);
-    
+    // light validation loop
     loop {
         println!("control on");
-        shifter.set(hvctl, 0b11111111, true);
+        hvcfg.update_ctl(0xFF, HvEngage::HvGenOff);
         delay_ms(500);
 
         println!("lockout toggle");
-        pin_lockout_n.high().unwrap(); // undo lockout
+        hvcfg.update_ctl(0xFF, HvEngage::HvGenOn);
         delay_ms(500);
-        pin_lockout_n.low().unwrap(); // redo lockout
+        hvcfg.update_ctl(0xFF, HvEngage::HvGenOff);
         delay_ms(500);
         
         println!("walk control");
         let mut shift_val = 1;
         for _ in 0..8 {
-            shifter.set(hvctl, shift_val, true);
+            hvcfg.update_ctl(shift_val, HvEngage::HvGenOff);
             shift_val <<= 1;
             delay_ms(500);
         }
         println!("control off");
-        shifter.set(hvctl, 0, true);
+        hvcfg.update_ctl(0, HvEngage::HvGenOff);
         delay_ms(500);
 
-        println!("lockout toggle");
-        pin_lockout_n.high().unwrap(); // undo lockout
+        println!("lockout toggle, voff");
+        hvcfg.update_ctl(0x00, HvEngage::HvGenOn);
         delay_ms(500);
-        pin_lockout_n.low().unwrap(); // redo lockout
+        hvcfg.update_ctl(0x00, HvEngage::HvGenOff);
         delay_ms(500);
 
         println!("row walk");
-        shift_val = 0b10000000;
-        for _ in 0..2 {
-            shifter.set(rowsel, shift_val, true);
-            shift_val >>= 1;
-            delay_ms(500);
-        }
-        shifter.set(rowsel, 0, true);
+        hvcfg.update_rowsel(RowSel::RowSel1);
+        delay_ms(500);
+        hvcfg.update_rowsel(RowSel::RowSel2);
+        delay_ms(500);
+        hvcfg.update_rowsel(RowSel::RowNone);
+        delay_ms(500);
 
         println!("col walk");
-        shift_val = 0b10000000;
-        for _ in 0..2 {
-            shifter.set(colsel, shift_val, true);
-            shift_val >>= 1;
-            delay_ms(500);
-        }
-        shifter.set(colsel, 0, true);
+        hvcfg.update_colsel(ColSel::ColSel1);
+        delay_ms(500);
+        hvcfg.update_colsel(ColSel::ColSel2);
+        delay_ms(500);
+        hvcfg.update_colsel(ColSel::ColNone);
+        delay_ms(500);
         
     }
 }
